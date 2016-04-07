37db04f9cf7c801af0f773f122c2972070a72458
ghsr <ghsr92@yandex.ru>
2016-04-07 23:32:19 +0600

    i9082: remove TARGET_CPU_SMP flag
    * TARGET_CPU_SMP defaults to true in Lollipop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
-TARGET_CPU_SMP := true
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
