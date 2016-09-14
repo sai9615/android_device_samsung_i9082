b31af29fb4482d4ec1a98eae9eae492b744fec6a
Milind <sai9615@gmail.com>
2016-09-14 21:33:58 +0530

    changes for N

--- system_prop.mk
+++ system_prop.mk
@@ -44,4 +44,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Disable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
-    ro.secure=0
+    ro.secure=0 \
+    ro.debuggable=1 \
+    persist.service.adb.enable=1
